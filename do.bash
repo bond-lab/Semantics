d71c6028c8b50533069c624e6d00829d819893fb
Francis Bond <bond@ieee.org>
2025-03-23 20:08:29 +0100

    update for 2025

--- do.bash
+++ do.bash
@@ -2,18 +2,35 @@ sdir=slides
 #subj=hg2052
 pushd .
 cd $sdir
-for slide in `ls hg8011-*.tex`
+#for slide in `ls hg8011-*.tex`
+# do
+#     base=`basename $slide .tex`
+#     echo Processing ${base}
+#     #latexmk -pdf -xelatex ${base}
+#     ## clean up
+#     #rm *.aux *.bbl *.blg *.log *~ *.dvi *.ps *.pdf
+# done
+
+for slide in `ls tut-*.tex`
 do
     base=`basename $slide .tex`
     echo Processing ${base}
-    latexmk -pdf -xelatex ${base}
+    ln -sf without.tex answers.tex
+    latexmk -silent -xelatex ${base}
+    mv "${base}.pdf" "${base}-questions.pdf"
+    echo Processing ${base} with answers
+    ln -sf with.tex answers.tex 
+    latexmk -silent -xelatex ${base}
+    mv "${base}.pdf" "${base}-answers.pdf"
     ## clean up
-    #rm *.aux *.bbl *.blg *.log *~ *.dvi *.ps *.pdf
+    #rm *.aux *.bbl *.blg *.log *~ 
 done
+
+
 popd
 
 ### copy changed slides
-rsync -av --ignore-times slides/*.pdf docs/pdf
+#rsync -av --ignore-times slides/*.pdf docs/pdf
 
 #htmldoc  --duplex --color --fontsize 12 --webpage -f /home/bond/papers/Outlines/${subj}-outline.pdf www/index.html
 
