1092ef0a40186999737bd41688a50853b5dce16f
Francis Bond <fcbond@gmail.com>
2025-04-01 10:48:56 +0200

    revisions for week 2

--- do.bash
+++ do.bash
@@ -30,7 +30,7 @@ done
 popd
 
 ### copy changed slides
-#rsync -av --ignore-times slides/*.pdf docs/pdf
+rsync -av --ignore-times slides/tut*questions.pdf docs/pdf
 
 #htmldoc  --duplex --color --fontsize 12 --webpage -f /home/bond/papers/Outlines/${subj}-outline.pdf www/index.html
 
